5bb57b3cd10f655df41864e88352d4a4d08d9d65
Ninja Hybrid Fornite <43005960+ShinobiDracoBalder@users.noreply.github.com>
2023-01-04 17:19:42 -0600

    Changed I
    First Origin

---
+++ CollectionViewDemo/MVVM/Models/ProductsGroup.cs
@@ -0,0 +1,12 @@
+﻿namespace CollectionViewDemo.MVVM.Models
+{
+    public class ProductsGroup : List<Product>
+    {
+        public string Name { get; set; }
+        public ProductsGroup(string name, List<Product> products)
+             : base(products)
+        {
+            Name = name;
+        }
+    }
+}
